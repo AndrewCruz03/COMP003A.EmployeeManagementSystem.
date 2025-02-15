1aaf5ee31b7991031ca66142379d8cd2d56b2d0b
AndrewCruz03 <Andrewcruzpacheco@my.whccd.edu>
2025-02-14 18:45:46 -0800

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.EmployeeManagementSystem_
+{
+    public class Employee
+       
+    {
+        private string _employeeId;
+        private string _firstName;
+        private string _middleName;
+        private string _lastName;
+        public double _salary;
+
+        
+
+
+        public string employeeId
+        {
+            get { return employeeId; }
+        }
+
+        public string firstName
+        {
+            get { return firstName; }
+        }
+        public string middleName
+        {
+            get { return middleName; }
+        }
+        public string lastName
+        {
+            get { return lastName; }
+        }
+
+    }
+}
+
+//string.IsNullOrWhiteSpace() for 1st and last name ,,2check thay are not empty or null
+
+
+//public double Balance
+//{
+//  get { return balance; }
+//set { if (value >= 0) balance = value; }
+
+// }
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Author: Andrew Cruz Pacheco
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Employee management system demonstrating OOP principles in C#
+using System.Security.Cryptography.X509Certificates;
+using COMP003A.EmployeeManagementSystem_;
+
+namespace COMP003A.EmployeeManagementSystem_
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+           
+
+
+
+
+        }
+    }
+}
+
+
+
